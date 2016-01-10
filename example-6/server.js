36224e45de37e68aef966898d8b7bc1906b80b02
Janzen Zarzoso <janzen.zarzoso@gmail.com>
2016-01-10 20:30:07 +0800

    Added oauth authentication.

--- example-6/server.js
+++ example-6/server.js
@@ -35,8 +35,8 @@ var generateToken = function(request, response) {
 	// is correct using JSONWebToken.verify
 
 	var payload = {
-		id: user.id,
-		username: user.username
+		id: request.user.id,
+		username: request.user.username
 	}
 
 	// Generate a random string
@@ -56,7 +56,6 @@ var generateToken = function(request, response) {
 }
 
 var generateTokenHandler = function(request, response) {
-	console.log(request.user);
 	var user = request.user;
 	// Generate our token
 	var token = generateToken(request, response);
@@ -111,6 +110,73 @@ var bearerStrategy = new BearerStrategy(verifyToken);
 
 Passport.use('bearer', bearerStrategy);
 
+var validateOAuth = function(accessToken, refreshToken, profile, done) {
+	var keys = Object.keys(users), user = null;
+	console.log(keys);
+
+	for (var iKey = 0; iKey < keys.length; iKey += 1) {
+		user = users[keys[iKey]];
+
+		if (user.thirdPartyId !== profile.user_id) {
+			continue;
+		}
+
+		return done(null, user);
+	}
+
+	users[profile.name] = user = {
+		username: profile.name,
+		id: keys.length,
+		thirdPartyId: profile.user_id
+	}
+
+	done(null, user);
+}
+
+var oAuthOptions = {
+	authorizationURL: 'https://janzen.auth0.com/authorize',
+	tokenURL: 'https://janzen.auth0.com/oauth/token',
+	clientID: 'oY7e57ac5axSrlCy4btpPSQ681CdPMHY',
+	clientSecret: 'z5S7JwO0Enm5gTG8rCetFZkDuMGpDU2M5jZkZ4Skr7ikiDf7r_mg7PKPn02rcKV1',
+	callbackURL: 'http://localhost:3000/oauth/callback'
+}
+
+var OAuth2Strategy = require('passport-oauth2').Strategy;
+
+var oAuthStrategy = new OAuth2Strategy(oAuthOptions, validateOAuth);
+
+var parseUserProfile = function(done, error, body) {
+	if (error) {
+		return done(new Error('Failed to fetch user profile'));
+	}
+
+	var json;
+
+	try {
+		json = JSON.parse(body);
+	} catch(error) {
+		return done(error);
+	}
+
+	done(null, json);
+}
+
+var getUserProfile = function(accessToken, done) {
+	oAuthStrategy._oauth2.get(
+			'https://janzen.auth0.com/userinfo',
+			accessToken,
+			parseUserProfile.bind(null, done)
+		)
+}
+
+oAuthStrategy.userProfile = getUserProfile;
+
+Passport.use('oauth', oAuthStrategy);
+
+app.get('/oauth', Passport.authenticate('oauth', {session: false}));
+
+app.get('/oauth/callback', Passport.authenticate('oauth', {session: false}), generateTokenHandler);
+
 // Routes
 app.post(
 	'/login',
@@ -132,6 +198,7 @@ app.get('/userinfo',
 		}
 );
 
+// Web Server
 app.listen(3000, function() {
 	console.log('Listening on port 3000');
 });
